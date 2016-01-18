9eb2c9f3370ed316fe2c1aa5993bc0d17dc60df5
LostAbaddon <lostabaddon@gmail.com>
2016-01-18 18:30:26 +0800

    修改首页；增加量子力学科普页

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+var Devices = {};
+(function (ua) {
+	Devices.isAndroid = !!ua.match(/Android/i);
+	Devices.isiOS = !!ua.match(/iPhone|iPad|iPod/i);
+	Devices.isBlackBerry = !!ua.match(/BlackBerry/i);
+	Devices.isOpera = !!ua.match(/Opera Mini/i);
+	Devices.isWindows = !!ua.match(/IEMobile/i) || !!ua.match(/WPDesktop/i);
+	Devices.isWebOS = !!ua.match(/webOS/i);
+	Devices.isMobile = Devices.isAndroid || Devices.isiOS || Devices.isBlackBerry || Devices.isOpera || Devices.isWindows;
+}) (navigator.userAgent);
