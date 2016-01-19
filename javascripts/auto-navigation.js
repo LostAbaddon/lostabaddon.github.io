8c54fc8e6180a9ff0a6a406b448562cff1b8ea03
LostAbaddon <lostabaddon@gmail.com>
2016-01-19 23:00:14 +0800

    Debug...

--- javascripts/auto-navigation.js
+++ javascripts/auto-navigation.js
@@ -1,19 +1,19 @@
 (function (root) {
 	root.AutoNavigation = function (selector) {
-		var dom = $(selector), mark = 'lostabaddon.github.io/articles/', index = root.location.pathname.indexOf(mark);
-		console.log(root.location.pathname, index);
+		var dom = $(selector), mark = 'lostabaddon.github.io/articles/', index = root.location.href.indexOf(mark);
+		console.log(root.location.href, index);
 		if (index < 0) {
 			dom.remove();
 			return;
 		}
-		var path = root.location.pathname.substring(index + mark.length, root.location.pathname.length).split('/');
+		var path = root.location.href.substring(index + mark.length, root.location.href.length).split('/');
 		path.pop();
 		path.unshift('home');
 		var result = "<span class='title'>当前位置：</span>", level = '';
 		path.map(function (nouse) {
 			level += '../';
 		});
-		if (root.location.pathname.indexOf('index.html') < 0) {
+		if (root.location.href.indexOf('index.html') < 0) {
 			level = level.replace('../', '');
 		}
 		path = path.map(function (path) {
