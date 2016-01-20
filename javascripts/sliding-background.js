8095d6a2aee59a774f0fce858e611473f6be2e8c
LostAbaddon <lostabaddon@gmail.com>
2016-01-20 22:00:29 +0800

    Update Sliding-Background

--- javascripts/sliding-background.js
+++ javascripts/sliding-background.js
@@ -9,16 +9,14 @@
 		else this.options.isVertical = (this.dom.data('direction').toLowerCase() === 'vertical');
 		if (!!options && !isEmpty(options.viewPerImage)) this.options.viewPerImage = options.viewPerImage;
 		else this.options.viewPerImage = this.dom.data('view-per-image') || 1;
-		if (this.options.isVertical) {
-			this.options.containerHeight = this.dom.height();
-		}
-		else {
-			this.options.containerWidth = this.dom.width();
-		}
+		this.options.containerHeight = this.dom.height();
+		this.options.containerWidth = this.dom.width();
 		if (!!options && !isEmpty(options.imageWidth)) this.options.imageWidth = options.imageWidth;
 		else this.options.imageWidth = this.dom.data('width');
 		if (!!options && !isEmpty(options.imageHeight)) this.options.imageHeight = options.imageHeight;
 		else this.options.imageHeight = this.dom.data('height');
+		if (!!options && !isEmpty(options.minScale)) this.options.minScale = options.minScale;
+		else this.options.minScale = this.dom.data('min-scale') || 0;
 
 		this.dom.find('img').each(function (index, img) {
 			self.images.push(new BackgroundController.BackgroundImage(img, self));
@@ -36,23 +34,31 @@
 		this.update();
 	};
 	BackgroundController.prototype.resetImages = function () {
-		var self = this;
+		var self = this, cw = self.options.containerWidth * self.options.minScale, ch = self.options.containerHeight * self.options.minScale;
 		this.images.map(function (img) {
-			img = img.dom;
-			var w, h, option = {};
+			var ui = img.dom, w, h, option = {};
 			h = self.options.imageHeight;
 			if (!isNaN(h)) h = (h * 100) + '%';
 			if (!!h) option.height = h;
 			w = self.options.imageWidth;
 			if (!isNaN(w)) w = (w * 100) + '%';
 			if (!!w) option.width = w;
-			img.css(option);
+			if (!option.width) option.width = "auto";
+			if (!option.height) option.height = "auto";
+			ui.css(option);
+			img.fitScale(cw, ch);
+			w = ui.data('width');
+			h = ui.data('height');
+			option = {};
 			if (self.options.isVertical) {
-				img.data('height', img.height());
+				w = (self.options.containerWidth - w) / 2;
+				option.left = w + 'px';
 			}
 			else {
-				img.data('width', img.width());
+				h = (self.options.containerHeight - h) / 2;
+				option.top = h + 'px';
 			}
+			ui.css(option);
 		});
 	};
 	BackgroundController.prototype.update = function () {
@@ -138,6 +144,34 @@
 			}
 		}
 		this.dom.css(css);
+	};
+	BackgroundController.BackgroundImage.prototype.fitScale = function (cw, ch) {
+		var w = this.img.width, h = this.img.height, option = {}, changed = false;
+		if (w < cw && h < ch) {
+			w = w / h * ch;
+			h = ch;
+			changed = true;
+		}
+		if (w < cw) {
+			h = h / w * cw;
+			w = cw;
+			changed = true;
+		}
+		else if (h < ch) {
+			w = w / h * ch;
+			h = ch;
+			changed = true;
+		}
+		if (changed) {
+			if (isFinite(w)) option.width = Math.round(w) + 'px';
+			else option.width = "auto";
+			if (isFinite(h)) option.height = Math.round(h) + 'px';
+			else option.height = "auto";
+		}
+		if (!!option.width || !!option.height) this.dom.css(option);
+		if (isNaN(w) || !isFinite(w)) w = this.dom.width();
+		if (isNaN(h) || !isFinite(h)) h = this.dom.height();
+		this.dom.data('width', w).data('height', h);
 	};
 
 	root.BackgroundController = BackgroundController;
