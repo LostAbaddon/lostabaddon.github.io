78dd11d554593755dabcac359edf2d85ce9e5970
LostAbaddon <lostabaddon@gmail.com>
2016-01-22 12:03:41 +0800

    DrawPad Simple Version DONE.

--- javascripts/drawpad.js
+++ javascripts/drawpad.js
@@ -35,8 +35,14 @@
 		};
 
 		// Private Methods
-		var drawing = false, old_x = 0, old_y = 0;;
+		var drawing = false, old_x = 0, old_y = 0, last_click = 0;
 		function start (x, y) {
+			var time = new Date().getTime();
+			if (time - last_click < 500) {
+				clear();
+				return;
+			}
+			last_click = time;
 			drawing = true;
 			old_x = x;
 			old_y = y;
@@ -68,23 +74,36 @@
 			old_y = y;
 		}
 		function clear () {
-
+			self.canvas.clearRect(0, 0, width, height);
 		}
 
 		// Events
-		this.button.on('click', this.close).on('tap', this.close);
-		this.drawarea.on('mousedown', function (e) {
-			start(e.offsetX, e.offsetY);
-		}).on('mouseup', function (e) {
-			end(e.offsetX, e.offsetY);
-		}).on('mousemove', function (e) {
-			moving(e.offsetX, e.offsetY);
-		}).on('doubleclick', function (e) {
-			console.log('Double Click');
-		});
-
-		// Test
-		this.open();
+		if (Devices.isMobile) {
+			var canvas_top = 0, canvas_left = 0;
+			this.button.on('tap', this.close);
+			this.drawarea.on('touchstart', function (e) {
+				var x = e.touches[0].clientX, y = e.touches[0].clientY;
+				canvas_top = self.drawarea[0].getBoundingClientRect();
+				canvas_left = canvas_top.left;
+				canvas_top = canvas_top.top;
+				start(x - canvas_left, y - canvas_top);
+			}).on('touchend', function (e) {
+				end(old_x, old_y);
+			}).on('touchmove', function (e) {
+				var x = e.touches[0].clientX, y = e.touches[0].clientY;
+				moving(x - canvas_left, y - canvas_top);
+			});
+		}
+		else {
+			this.button.on('click', this.close);
+			this.drawarea.on('mousedown', function (e) {
+				start(e.offsetX, e.offsetY);
+			}).on('mouseup', function (e) {
+				end(e.offsetX, e.offsetY);
+			}).on('mousemove', function (e) {
+				moving(e.offsetX, e.offsetY);
+			});
+		}
 	}
 
 	root.DrawPad = DrawPad;
